9d617a87c959f1b2ffc9d626452c94d9cf6f77d8
Maria Kim <maria.kim@gmail.com>
2018-10-23 00:23:59 +0000

    Stock restrictions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,19 @@ class User < ActiveRecord::Base
   
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    # is the stock tracked by the user?
+    user_stocks.where(stock_id: stock.id).exists?
+  end
+  
+  def under_stock_limit?
+    (user_stocks.count < 10)
+  end
+  
+  def can_add_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+  end
 end
