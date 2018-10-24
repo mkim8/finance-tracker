73145c7063c58fd9f0d1d476c0d90d91ba7abd4a
Maria Kim <maria.kim@gmail.com>
2018-10-24 22:34:46 +0000

    Add and remote friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,14 @@ class UsersController < ApplicationController
   end
   
   def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+    if current_user.save
+      flash[:success] = "friend was successfully added"
+    else
+      flash[:danger] = "There was something wrong with the friend request"
+    end
+    redirect_to my_friends_path
   end
 
 end
