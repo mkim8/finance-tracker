ca85e6805f36674d9d116f44c6429c44fe06d7e9
Maria Kim <maria.kim@gmail.com>
2018-10-24 18:44:18 +0000

    Adding search methods for my friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
   def my_friends
     @friendships = current_user.friends
   end
+  
+  def search
+    @users = User.search(params[:search_param])
+    render json: @users 
+  end
+
 end
